b93db4dcfb6b1caebc6985e49b864bfaf8164743
nah <qi.qui.quijano@gmail.com>
2015-11-22 17:43:55 -0800

    The first lesson

---
+++ 11-19-15 Objects.js
@@ -0,0 +1,71 @@
+function(){
+}
+
+if(true){
+}
+
+var person = {
+	'name' : 'josh haglun',
+	'age' : 37,
+	'email' : 'jhaglun@gmail.com',
+	'siblings : [ 'jake', 'brook', 'alexis', 'mckenzie', 'adam'],
+	'job' : 'programmer',
+	'doSomething' : function(){
+		//this function does stuff
+		
+	}
+};
+
+person.doSomething();
+
+/*
+ this is a block comment
+*/
+
+//this is a regular comment
+
+var friends = [
+	{
+	'name' : 'josh haglun',
+	'age' : 37,
+	'email' : 'jhaglun@gmail.com',
+	'siblings : [ 'jake', 'brook', 'alexis', 'mckenzie', 'adam'],
+	'job' : 'programmer'
+	},{
+	'name' : 'josh haglun',
+	'age' : 37,
+	'email' : 'jhaglun@gmail.com',
+	'siblings' : [ 'jake', 'brook', 'alexis', 'mckenzie', 'adam'],
+	'job' : 'programmer'
+	},{
+	'name' : 'josh haglun',
+	'age' : 37,
+	'email' : 'jhaglun@gmail.com',
+	'siblings : [ 'jake', 'brook', 'alexis', 'mckenzie', 'adam'],
+	'job' : 'programmer'
+	},{
+	'name' : 'josh haglun',
+	'age' : 37,
+	'email' : 'jhaglun@gmail.com',
+	'siblings : [ 'jake', 'brook', 'alexis', 'mckenzie', 'adam'],
+	'job' : 'programmer'
+	},{
+	'name' : 'josh haglun',
+	'age' : 37,
+	'email' : 'jhaglun@gmail.com',
+	'siblings : [ 'jake', 'brook', 'alexis', 'mckenzie', 'adam'],
+	'job' : 'programmer'
+	},{
+	'name' : 'josh haglun',
+	'age' : 37,
+	'email' : 'jhaglun@gmail.com',
+	'siblings : [ 'jake', 'brook', 'alexis', 'mckenzie', 'adam'],
+	'job' : 'programmer'
+	}
+];
+
+console.log( 'name', person.name);
+
+var friends = ['josh' , 'michael', 'fuzz', 'krista', {}]
+
+
